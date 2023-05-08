b2bdc7f81d4733a41769e1a0d8da7078e77a35ef
Archisman <archisman.mukherjee02@gmail.com>
2023-05-08 23:52:55 +0530

    made controllers, routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-console.log("router loaded");
+const homeController = require("../controllers/home_controller");
 
+console.log("router loaded");
+router.get("/", homeController.home);
 module.exports = router;
